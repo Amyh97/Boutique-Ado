343168a2a2666c2faa0c33eec1c1455db46e8061
Amyh97 <amy.hollis@btinternet.com>
2021-01-14 13:19:18 +0000

    setup allauth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount'
 ]
 
 MIDDLEWARE = [
@@ -59,13 +63,32 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # tells allauth we went to allow authentication from either usernames or email
+ACCOUNT_EMAIL_REQUIRED = True  # email is needed to create an account 
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # email has to be varified to create account
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True  # have to type twice to avoid typos
+ACCOUNT_USERNAME_MIN_LENGTH = 4  # does what it says
+LOGIN_URL = '/accounts/login/'  # specifies login url
+LOGIN_REDIRECT_URL = '/'  # URL to redirect to on a scuuessful login, temporarily redirect to /success to test.
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
