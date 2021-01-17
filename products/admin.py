d7b10068d3b173a5f097b85e4ed67b165ad90eb6
Amyh97 <amy.hollis@btinternet.com>
2021-01-17 13:44:58 +0000

    customise admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ( # list display attribute is what is seen in admin pannel 
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image'
+    )
+
+    ordering = ('sku',) # must be tuple
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
