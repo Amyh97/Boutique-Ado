9fb2fef04cd68fafbc4a1694d40df9cf4564a10a
Amyh97 <amy.hollis@btinternet.com>
2021-01-17 19:07:57 +0000

    added categories filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -11,8 +11,15 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            # double underscore is common Django syntax for queries
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,10 +28,11 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)  # pipe means or, i in contains removes case sensitivity
             products = products.filter(queries)
-            
+
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories
     }
 
     return render(request, 'products/products.html', context)
