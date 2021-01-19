f76d518a7b050dbc1143665a1fc5af61fc187340
Amyh97 <amy.hollis@btinternet.com>
2021-01-19 15:13:15 +0000

    added bag context and delivery logic

---
+++ bag/contexts.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+from django.conf import settings
+
+# context processor creates dictionary that is avalible across the whole application, add to settings.py
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        # decimal used rather than float as float is more susceptible to errors
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total
+    }
+
+    return context
