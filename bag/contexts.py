217f8284ae7ea6877ea385734999b3920bb27b19
Amyh97 <amy.hollis@btinternet.com>
2021-01-20 11:22:54 +0000

    finished product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -13,15 +13,28 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():  # bag from session
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product
-        })
+    for item_id, item_data in bag.items():  # bag from session
+        if isinstance(item_data, int):  # aka has no sizes
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size
+                })
+
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         # decimal used rather than float as float is more susceptible to errors
--- bag/views.py
+++ bag/views.py
@@ -10,14 +10,26 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    # session will persist while user continues to browse
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
 
+    # session will persist while user continues to browse
     bag = request.session.get('bag', {})  # get it if it existis or create one if it doesn't
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity  # if already in the bag, adds quantity
-    else:
-        bag[item_id] = quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():  # if item is in bag add how ever many
+                bag[item_id]['items_by_size'][size] += quantity
+            else:  # if not already got that size in the bag, add that size to bag 
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else:  # if size is N/A
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity  # if already in the bag, adds quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag  # updated version of bag
     return redirect(redirect_url)
