f591473feefc2c44b26fc0ee1350d08add9da99a
Amyh97 <amy.hollis@btinternet.com>
2021-01-26 16:10:20 +0000

    Added profile form, views and updated templates

--- profiles/models.py
+++ profiles/models.py
@@ -10,15 +10,22 @@ class UserProfile(models.Model):
     """
     A user profile model for maintaining default
     delivery information and order history
-    each user can only have 1 profile and 1 per person
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_country = CountryField(blank_label='Counrty *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20,
+                                            null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40,
+                                            null=True, blank=True)
+    default_county = models.CharField(max_length=80,
+                                      null=True, blank=True)
+    default_postcode = models.CharField(max_length=20,
+                                        null=True, blank=True)
+    default_country = CountryField(blank_label='Country',
+                                   null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -27,9 +34,9 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or update profile
+    Create or update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # Existing users: just save profile
+    # Existing users: just save the profile
     instance.userprofile.save()
