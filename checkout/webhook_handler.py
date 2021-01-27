a33e282479de1ab677412fd81d5b4358ca16294b
Amyh97 <amy.hollis@btinternet.com>
2021-01-27 11:56:53 +0000

    updqated webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -34,13 +35,33 @@ class StripeWH_Handler:
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount / 100, 2)
+
         # stripe will store blank strings if no data entered, not "None"
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+        # Update profile information if save_info  was checked
+        profile = None  # still guest checkout
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone,
+                profile.default_country = shipping_details.address.country,
+                profile.default_postcode = shipping_details.address.\
+                    postal_code,
+                profile.default_town_or_city = shipping_details.address.city,
+                profile.default_street_address1 = shipping_details.address.\
+                    line1,
+                profile.default_street_address2 = shipping_details.address.\
+                    line2,
+                profile.default_county = shipping_details.address.state,
+                profile.save()
+
         # add order to db in case of error like page close before success
         order_exists = False
-        """if not found wait one second (line62) cause delay incase view is slower
+        """if not found wait one second (line86) cause delay incase view is slower
         than webohook """
         attempt = 1
         while attempt <= 5:
@@ -58,7 +79,7 @@ class StripeWH_Handler:
                     street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
-                    # identify correct order incase customer makes identical orders
+                    # identify correct order if customer makes identical orders
                     original_bag=bag,
                     stripe_pid=pid,
                 )
@@ -78,6 +99,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                             full_name=shipping_details.name,
+                            user_profile=profile,
                             email=billing_details.email,
                             phone_number=shipping_details.phone,
                             country=shipping_details.address.country,
@@ -91,23 +113,24 @@ class StripeWH_Handler:
                             stripe_pid=pid,
                         )
                 for item_id, item_data in json.loads(bag).items():
-                        product = Product.objects.get(id=item_id)
-                        if isinstance(item_data, int):
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size']\
+                                .items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
-                                quantity=item_data,
+                                quantity=quantity,
+                                product_size=size,
                             )
                             order_line_item.save()
-                        else:
-                            for size, quantity in item_data['items_by_size'].items():
-                                order_line_item = OrderLineItem(
-                                    order=order,
-                                    product=product,
-                                    quantity=quantity,
-                                    product_size=size,
-                                )
-                                order_line_item.save()
             except Exception as e:
                 # if anything goes wrong, delete
                 if order:
