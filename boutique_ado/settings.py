003a7150bfa77a3dd2dbf338f4b75329c9d3f25f
Amyh97 <amy.hollis@btinternet.com>
2021-01-28 18:29:49 +0000

    added cahce control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -179,6 +179,11 @@ MEDIA_URL = ('/media/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control - tells browser its ok to cache for a long time
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:000 GMT',
+        'CacheControl' : 'max-age=94608000',
+    }
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-amy'
     AWS_S3_REGION_NAME = 'eu-west-2'
